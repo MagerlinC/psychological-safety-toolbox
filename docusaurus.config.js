8ff0909287de3400dd439370724d11af9448bd18
mikkel agerlin <mikkel_ac@hotmail.com>
2023-10-22 11:51:10 +0200

    fix: github link 2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
               },
               {
                 label: "GitHub",
-                href: "https://github.com/facebook/docusaurus",
+                href: "https://github.com/MagerlinC/psychological-safety-toolbox/tree/main",
               },
             ],
           },
